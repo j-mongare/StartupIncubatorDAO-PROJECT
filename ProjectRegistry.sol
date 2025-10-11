76c87fbdbc313fd12d33dedb20c634d6042a93c3
Jefferson Mongare <117610287+j-mongare@users.noreply.github.com>
2025-10-12 01:12:32 +0300

    Update ProjectRegistry.sol

--- ProjectRegistry.sol
+++ ProjectRegistry.sol
@@ -12,20 +12,29 @@ contract ProjectRegistry {
         string description;
         bool approved;
     }
+//==============storage===============
 
     uint public nextProjectId;
     mapping(uint => Project) public projects;
     address public manager;
 
+//==============events=================
     event ProjectRegistered(uint indexed id, address owner, string name);
     event ProjectApproved(uint indexed id, address indexed approver);
+
+//====================custom errors================
 
     error NotManager();
     error InvalidProject();
 
+//=================constructor===========================
+
     constructor(address _manager) {
         manager = _manager;
     }
+//======================core logic==========================
+//@notice anyone can call this function and register their project
+// @param calldata for external input. Also cheaper than memory
 
     function registerProject(string calldata name, string calldata description) external {
         uint id = nextProjectId;
@@ -41,6 +50,7 @@ contract ProjectRegistry {
 
         emit ProjectRegistered(id, msg.sender, name);
     }
+//@notice only the manager can approve a project.
 
     function approveProject(uint id) external {
         if (msg.sender != manager) revert NotManager();
@@ -54,6 +64,7 @@ contract ProjectRegistry {
 
         emit ProjectApproved(id, msg.sender);
     }
+//=============helper functions=================
 
     function getProject(uint id) external view returns (Project memory) {
         return (projects[id]);
