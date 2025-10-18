d7f1973fe0c79c1fa03510c31c9cf13c51393e5d
Jefferson Mongare <117610287+j-mongare@users.noreply.github.com>
2025-10-18 22:40:31 +0300

    Add _Transfer event in mint function
    Added _Transfer event to track minting operations.

--- IncubatorToken.sol
+++ IncubatorToken.sol
@@ -9,13 +9,17 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract IncubatorToken is ERC20, AccessControl {
 // @notice hashing the MINTER_ROLE to ensure subsequent minters each have unique identifiers
+ bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE"); 
 
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE"); 
+event _Transfer(address indexed _from, address indexed to, uint256 amount);
+
     constructor() ERC20("Incubator Reputation Token", "IREP") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender); // deployer is the default admin
     }
 
     function mint(address to, uint amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+      address _from = address(0);
+      emit _Transfer(_from, to, amount);
     }
 }
